d3fed85adf0f9abba20879b6e3d5afa0daaf065e
Christoph Conrads <gitlab@christoph-conrads.name>
2016-09-16 19:08:32 +0200

    Use Fibonacci sequence generator

--- main.go
+++ main.go
@@ -5,11 +5,47 @@ import (
 	"strconv"
 	"os"
 	"math/big"
+	"fizzbuzz_prime/fibonacci"
 )
 
 
 func IsZero(i *big.Int) bool {
 	return i.Sign() == 0
+}
+
+
+
+func fizzbuzz(n uint64) {
+	fibGen := fibonacci.MakeGenerator()
+	r := big.NewInt(-1)
+	i3 := big.NewInt(3)
+	i5 := big.NewInt(5)
+
+	for i := uint64(1); i <= n; i++ {
+		f := fibGen.Execute()
+
+		if f.ProbablyPrime(50) {
+			fmt.Printf("BuzzFizz\n")
+			continue
+		}
+
+		r.Mod(f, i3)
+		has_factor3 := IsZero(r)
+
+		r.Mod(f, i5)
+		has_factor5 := IsZero(r)
+
+		switch {
+		case has_factor3 && has_factor5:
+			fmt.Printf("FizzBuzz\n")
+		case has_factor3:
+			fmt.Printf("Buzz\n")
+		case has_factor5:
+			fmt.Printf("Fizz\n")
+		default:
+			fmt.Printf( "%s\n", f.String() )
+		}
+	}
 }
 
 
@@ -29,46 +65,6 @@ func main() {
 	}
 
 	n := uint64(input)
+	fizzbuzz(n)
 
-
-	if n > 0 {
-		fmt.Printf("1\n")
-	}
-	if n <= 1 {
-		os.Exit(0)
-	}
-
-
-	a := big.NewInt(0)
-	b := big.NewInt(1)
-	r := big.NewInt(-1)
-	i3 := big.NewInt(3)
-	i5 := big.NewInt(5)
-
-	for i := uint64(2); i <= n; i++ {
-		a.Add(a, b)
-		a, b = b, a
-
-		if b.ProbablyPrime(50) {
-			fmt.Printf("BuzzFizz\n")
-			continue
-		}
-
-		r.Mod(b, i3)
-		has_factor3 := IsZero(r)
-
-		r.Mod(b, i5)
-		has_factor5 := IsZero(r)
-
-		switch {
-		case has_factor3 && has_factor5:
-			fmt.Printf("FizzBuzz\n")
-		case has_factor3:
-			fmt.Printf("Buzz\n")
-		case has_factor5:
-			fmt.Printf("Fizz\n")
-		default:
-			fmt.Printf( "%s\n", b.String() )
-		}
-	}
 }
