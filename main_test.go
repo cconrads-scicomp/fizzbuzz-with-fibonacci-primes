6f50d1a1138f5f23dd7837d7ef8af79a7e42e5e7
Christoph Conrads <gitlab@christoph-conrads.name>
2016-09-16 19:08:49 +0200

    Test program output

---
+++ main_test.go
@@ -0,0 +1,30 @@
+package main
+
+
+func Example() {
+	// f1, f2, ... =
+	//    1,    1,    2,    3,    5,    8,   13,   21,   34,   55,   89,  144,
+	//  233,  377,  610,  987, 1597, 2584, 4181, 6765,
+	fizzbuzz(20)
+	// Output:
+	// 1
+	// 1
+	// BuzzFizz
+	// BuzzFizz
+	// BuzzFizz
+	// 8
+	// BuzzFizz
+	// Buzz
+	// 34
+	// Fizz
+	// BuzzFizz
+	// Buzz
+	// BuzzFizz
+	// 377
+	// Fizz
+	// Buzz
+	// BuzzFizz
+	// 2584
+	// 4181
+	// FizzBuzz
+}
