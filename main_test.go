0e2638c1a68c50becda3ea5146dd5dedc4f33781
Christoph Conrads <gitlab@christoph-conrads.name>
2016-09-17 13:36:08 +0200

    Add benchmark for probabilistic primality test

--- main_test.go
+++ main_test.go
@@ -1,6 +1,12 @@
 package main
 
 
+import (
+	"fizzbuzz_prime/fibonacci"
+	"testing"
+)
+
+
 func Example() {
 	// f1, f2, ... =
 	//    1,    1,    2,    3,    5,    8,   13,   21,   34,   55,   89,  144,
@@ -27,4 +33,25 @@ func Example() {
 	// 2584
 	// 4181
 	// FizzBuzz
+}
+
+
+func BenchmarkFibonacciPrimeTest(b *testing.B) {
+	b.StopTimer()
+	m := 50
+	n0 := 1000
+
+	for i := 0; i < b.N; i++ {
+		gen := fibonacci.MakeGenerator()
+		for n := 0; n < n0; n++ {
+			gen.Execute()
+		}
+
+		b.StartTimer()
+		for n := n0; n < 2000; n++ {
+			f := gen.Execute()
+			f.ProbablyPrime(m)
+		}
+		b.StopTimer()
+	}
 }
